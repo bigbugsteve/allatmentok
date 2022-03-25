22930c77a090fcfb69157f1e27e0df6b4b3a13cd
Gábor Nagy <gabor.nagy@ewmgroup.com>
2022-03-25 15:04:20 +0100

    align navigation

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,32 +1,34 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <div class="container w-75 m-auto">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Rólunk <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Hogyan segíthetsz</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Hírek</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Események</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Projektek</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Partnerek</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Kapcsolat</a>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse m-auto" id="navbarSupportedContent">
+                <ul class="navbar-nav m-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Rólunk <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Hogyan segíthetsz</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Hírek</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Események</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projektek</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Partnerek</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kapcsolat</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
