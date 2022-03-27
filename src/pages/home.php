d59fafc5fb7bf114e9291742a6c3cdfa38fbd5f4
Adam Kovacs <kovacs.adam@itexpress.hu>
2022-03-27 16:08:45 +0200

    homepage V1

--- src/pages/home.php
+++ src/pages/home.php
@@ -1,1 +1,58 @@
-<div>Hello</div>
+
+<div class="global-container">
+    <img class="cover" src="assets/img/cover.png" width=100%>
+
+    <div class="buttons bg-blue text-white padding30">
+        BUTTONS
+    </div>
+
+    <div class="bg-purple text-white">
+        <div class="left">
+            <img src="assets/img/tetovalt_logos.png" alt="">
+        </div>
+        <div class="wrapper400">
+        <div class="text-size-medium"><p>A Tetovált Állatmentők</p></div>
+        <p>2010-ben alakultunk meg Budapesti Állatmentők néven. Az állatvédelemben önállóan tevékenykedő tagjaink puszta elhivatottságból, állatszeretetből, önerőből, szervezett formában kezdték menteni a borzalmas körülmények között élő megkínzott, elhagyott, valamint balesetet szenvedett állatokat. Magyarországon erre sajnos a nagymértékben jelen lévő emberi brutalitás és állatkínzások miatt folyamatosan szükség van, az igény nem csökkent. Jelenleg a kötelező tíz fő egyesületi taglétszámon túl 30 aktív önkéntesünkkel látjuk el feladatainkat.</p>
+
+        <p>2011. májusában Tetovált Állatmentők Állatvédelmi Egyesületként kerültünk bejegyzésre, mint a Budapesti Állatmentők utóda. A civil csapat folyamatosan bővült, és számtalan állat sorsának jobbra fordulását kísértük figyelemmel. Egyrészt az állat gazdájának együttműködő magatartásával - amikor belátta, hogy az állatot maga tartani, ellátni képtelen és lemondott a tulajdonjogról - másrészt ellenállás esetén hatósági eljárás kezdeményezésével, rendőri segítség bevonásával. A legtöbb fővárosi kerületben már kialakult, jól működő kapcsolatunk van a rendőrséggel, polgármesteri hivatalokkal, a hatósági állatorvosokkal. Mentett állatainkkal gyakran az utolsó pillanatban érkezünk az állatorvosokhoz. A csapat erre kijelölt további tagjai pedig azon dolgoznak, hogy ideiglenes vagy végleges gazdát találjanak a pártfogásba vett állatoknak.</p>
+
+        <p>Elsősorban Budapesten és Pest megyében tevékenykedünk, de szükség esetén az ország bármely pontjára hívhatóak vagyunk. Nevünk és tevékenységünk híre egyre jobban terjedt az elmúlt években, és beigazolódott, hogy nagy szükség van a munkánkra, és természetesen minden állatvédő személy, szervezet munkájára is, hiszen a bejelentések száma jelentősen nőtt az elmúlt időszakban.</p>
+
+        <p>Az elmúlt években több száz állatot sikerült megmentenünk, rehabilitálnunk, gazdához segítenünk. Évről-évre több pozitív visszajelzést és támogatást kapunk az emberektől, akik már ismerik a munkánkat, elégedettek szervezetünkkel. A pozitív visszaigazolások a szervezet évenkénti 1%-os támogatásának növekedésében is megmutatkoznak.</p>
+    </div>
+    <div class="bg-white text-blue text-center padding30">
+        <div class="text-size-large"><p>ESEMÉNYEK</p></div>
+        <div class="text-size-medium">
+            <a href="http://">LINK</a><br>
+            <a href="http://">LINK</a><br>
+            <a href="http://">LINK</a><br>
+            <a href="http://">LINK</a><br>
+        </div>
+    </div>
+    <div class="bg-blue text-white padding30 text-center">
+        <div class="text-size-large"><p>VIDEÓK</p></div>
+        <table class="table-center">
+            <tr>
+                <th>Sajat video</th>
+                <th>YouTube video</th>
+            </tr>
+            <tr>
+                <th><video controls width="560">
+                    <source src="video/flower.mp4" type="video/mp4">
+                    Sorry, your browser doesn't support embedded videos.
+                    </video></th>
+                <th><iframe width="560" height="315" src="https://www.youtube.com/embed/i810CxN5Q6Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></th>
+            </tr>
+        </table>
+    </div>
+    <div class="bg-white text-blue padding30">
+    <div class="text-size-large text-center"><p>ITT TALÁLSZ MINKET</p></div>
+        <div class="left560">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5393.29045309108!2d19.346605027617773!3d47.477342516704965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741c75aa8335fbd%3A0x2b865e080321c82!2zVGV0b3bDoWx0IMOBbGxhdG1lbnTFkWs!5e0!3m2!1shu!2shu!4v1648303439198!5m2!1shu!2shu" width="560" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="wrapper600">
+        Tetovált Állatmentők
+        <br>Pécel, 92hrsz, külterület, 0384
+        </div>
+    </div>
+</div>
