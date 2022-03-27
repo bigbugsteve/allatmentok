a559caf7ffd3466518a854e7eb1de770d03ad320
Adam Kovacs <kovacs.adam@itexpress.hu>
2022-03-27 18:20:36 +0200

    update pages/contact.php

--- src/pages/contact.php
+++ src/pages/contact.php
@@ -1,23 +1,27 @@
 <div class="global-container">
 	<div class="card login-form bg-purple text-white">
 	<div class="card-body">
-		<h3 class="card-title text-center">Bejelentkezés</h3>
+		<h3 class="card-title text-center">Uzenet</h3>
 		<div class="card-text">
-			<form>
+		<form name="kapcsolat" action="contact.php" onsubmit="return ellenoriz();" method="post">
+
 				<div class="form-group">
-					<label for="exampleInputEmail1">Email</label>
-					<input type="email" class="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp">
+					<label for="exampleInputName">Nev</label>
+					<input class="form-control form-control-sm" input type="text" id="nev" name="nev" size="20" maxlength="40">
 				</div>
+
 				<div class="form-group">
-					<label for="exampleInputPassword1">Jelszó</label>
-					<!-- <a href="#" style="float:right;font-size:12px;">Forgot password?</a> -->
-					<input type="password" class="form-control form-control-sm" id="exampleInputPassword1">
+					<label for="exampleInputEmail">Email</label>
+					<input class="form-control form-control-sm" input type="text" id="email" name="email" size="30" maxlength="40">
 				</div>
-				<button type="submit" class="btn bg-white text-blue btn-block">Bejelentkezés</button>
-				
-				<div class="sign-up d-flex justify-content-between align-items-center mt-3">
-					Nincs még fiókja? <a href="?page=registration" class="bg-white btn p-1">Regisztráció</a>
+
+				<div class="form-group">
+					<label for="exampleInputMessage">Uzenet</label>
+					<textarea id="szoveg" name="szoveg" cols="46" rows="10"></textarea>
 				</div>
+				<button onclick="ellenoriz()" type="button" class="btn bg-white text-blue btn-block">Ellenőriz</button>
+				<input id="kuld" type="submit" value="Küld" class="btn bg-white text-blue btn-block">
+
 			</form>
 		</div>
 	</div>
