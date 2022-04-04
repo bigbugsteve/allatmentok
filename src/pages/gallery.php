e6952b8e2582fa08b70260dc261f9ce379618bf7
Adam Kovacs <kovacs.adam@itexpress.hu>
2022-04-04 11:41:12 +0200

    gallery final version

--- src/pages/gallery.php
+++ src/pages/gallery.php
@@ -15,28 +15,7 @@ closedir($reader);
 <div class="bg-purple text-white text-center text-size-large">
     <h1 class="page-title">Galéria</p>
 </div>
-<div class="gallery">
-    <?php
-    arsort($images);
-    foreach ($images as $file => $date) {
-    ?>
-        <div class="picture">
-            <div class="card" style="width: 18rem; margin: 10px;">
-                <img src="<?php echo $GALLERY . $file ?>" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Név: <?php echo $file; ?></h5>
-                    <!-- <p class="card-text">Dátum:  <?php echo date($DATEFORM, $date); ?></p> -->
-                    <a href="<?php echo $GALLERY . $file ?>" class="btn btn-primary bg-purple w-100">Megnyitás</a>
-                </div>
-            </div>
 
-        </div>
-    <?php
-    }
-    ?>
-</div>
-
-<h2>V2:</h2>
 <div class="gallery">
     <?php
         arsort($images);
