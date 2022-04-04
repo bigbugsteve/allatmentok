03036905f764cab323bfb7da9d14c90aeff6c879
Adam Kovacs <kovacs.adam@itexpress.hu>
2022-04-04 18:41:37 +0200

    update help.php

--- templates/sections/help.php
+++ templates/sections/help.php
@@ -9,8 +9,9 @@
                         <i class="text-purple bi bi-check2"></i>
                     </div>    
                 </div>
-                <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">Ments Te is életet, támogasd alapítványunk munkáját adód 1%-ával!<br>
-Az ikonra kattintva letöltheted a nyomtatható nyilatkozatot.</div>
+                <div class="col-xs-12 col-md-5"> Ments Te is életet, támogasd alapítványunk munkáját adód 1%-ával!<br>
+                Az ikonra kattintva letöltheted a nyomtatható nyilatkozatot.
+                </div>
             </li>
             <li class=" border-0 row py-2">
                 <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">ADHAT vonal</div>
@@ -20,7 +21,8 @@ Az ikonra kattintva letöltheted a nyomtatható nyilatkozatot.</div>
                     </div>    
                 </div>
                 <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">Hívd fel az 13600-as telefonszámot és üsd be az 55-ös melléket, vagy küldd az 55-ös számot az 13600-as telefonszámra (csak a számot!) <br>
-    A támogatás mértéke 500 Ft / hívás, SMS</div>
+                A támogatás mértéke 500 Ft / hívás, SMS
+                </div>
             </li>
             <li class=" border-0 row py-2">
                 <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">PayPal</div>
@@ -29,24 +31,20 @@ Az ikonra kattintva letöltheted a nyomtatható nyilatkozatot.</div>
                     <i class="bi bi-paypal"></i>
                     </div>    
                 </div>
-                <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">Felajánlásaitokat az info@allatmentok.hu PayPal fiókra tudjátok elküldeni.</div>
+                <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">Felajánlásaitokat az info@allatmentok.hu PayPal fiókra tudjátok elküldeni. </div>
             </li>
             <li class=" border-0 row py-2">
                 <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">Banki átutalással</div>
                 <div class="col-xs-12 col-md-2 justify-content-center d-flex align-items-center help__icon">
                     <div class="help__icon-wrapper">
-                        <i class="text-purple bi bi-credit-card"></i>
+                    <i class="text-purple bi bi-credit-card"></i>
                     </div>    
                 </div>
-                <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">Bankszámla szám: 10918001-00000071-62750004 <br>
-
-Külföldről: IBAN: HU94 1091 8001 0000 0071 6275 0004 <br>
-
-EUR számlánk: HU02 1091 8001 0000 0071 6275 0011 <br>
-
-SWIFT: <span class="text-uppercase">
-BACXHUHB
-</span> </div>
+                <div class="col-xs-12 col-md-5">Bankszámla szám: 10918001-00000071-62750004<br>
+                Külföldről: IBAN: HU94 1091 8001 0000 0071 6275 0004<br>
+                EUR számlánk: HU02 1091 8001 0000 0071 6275 0011<br>
+                SWIFT: BACXHUHB
+                </div>
             </li>
             <li class=" border-0 row py-2">
                 <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">Tárgyi adományok felajánlásával</div>
@@ -55,13 +53,8 @@ BACXHUHB
                     <i class="text-purple bi bi-telephone"></i>
                     </div>    
                 </div>
-                <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">Ami számodra feleslegessé válik, lehet pont az hiányzik épülő rehabilitációs központunkból! <br>
-
-Védenceink gondozásához segítséget jelent egy-egy meleg takaró, ruha, kinőtt, megunt, vagy feleslegessé vált állatfelszerelés, kutyaház, hordozó, kennel, stb. Mentett állatkáink ellátását tápokkal is segítheted, akár 1-1 konzerv, vagy 1 zacskó száraztáp, vagy megmaradt gyógyszer is hatalmas segítség lehet! Mint ahogy fentebb is írtuk: Minden apró segítség számít, nincs olyan, hogy kevés! Kenneleink építéséhez építőanyagot, fémet, hullámpalát, faanyagok, régi szőnyeget, vagy bármi még használható felajánlást szívesen fogadunk. <br>
-
-Tárgyi adományaidat személyesen is eljuttathatod részünkre, a különböző rendezvényeken egyesületünk önkéntesei készséggel állnak rendelkezésre, és átveszik azokat. <br>
-
-Nagyobb mennyiség, vagy építőanyag esetén, illetve ha bármi kérdés felmerülne, keress minket bizalommal az info@allatmentok.hu címen keresztül, vagy a facebook-on!</div>
+                <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center"> Ami számodra feleslegessé válik, lehet pont az hiányzik épülő rehabilitációs központunkból! <br> Védenceink gondozásához segítséget jelent egy-egy meleg takaró, ruha, kinőtt, megunt, vagy feleslegessé vált állatfelszerelés, kutyaház, hordozó, kennel, stb. Mentett állatkáink ellátását tápokkal is segítheted, akár 1-1 konzerv, vagy 1 zacskó száraztáp, vagy megmaradt gyógyszer is hatalmas segítség lehet! Mint ahogy fentebb is írtuk: Minden apró segítség számít, nincs olyan, hogy kevés! Kenneleink építéséhez építőanyagot, fémet, hullámpalát, faanyagok, régi szőnyeget, vagy bármi még használható felajánlást szívesen fogadunk. <br> Tárgyi adományaidat személyesen is eljuttathatod részünkre, a különböző rendezvényeken egyesületünk önkéntesei készséggel állnak rendelkezésre, és átveszik azokat. <br> Nagyobb mennyiség, vagy építőanyag esetén, illetve ha bármi kérdés felmerülne, keress minket bizalommal az info@allatmentok.hu címen keresztül, vagy a facebook-on!
+                </div>
             </li>
             <li class=" border-0 row py-2">
                 <div class="col-xs-12 col-md-5 justify-content-center d-flex align-items-center">Tetkós termékek vásárlásával</div>
